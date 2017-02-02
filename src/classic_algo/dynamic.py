a7a2fed3d2bcd9bf0162397e17052ec6a8ac4754
Arnaud Blois <a.blois@ucl.ac.uk>
2017-02-02 11:06:27 +0000

    added change_making in classic_algo.dynamic

--- src/classic_algo/dynamic.py
+++ src/classic_algo/dynamic.py
@@ -14,11 +14,18 @@ to solve this type of problems:
 on them to reach the complete problem
 
 * the top-down approach where the larger problem is broken down into
-subproblems, this often leads to slightly more efficient and readable solution.
+subproblems, and each result is memoized (ie kept in memory) to avoid
+unnecessary recomputation. This approach often leads to slightly more efficient
+and readable solution.
 
-A few typical example are presented in this module
+A few typical example are presented in this module.
 **[This section is currently under construction]**
 """
+
+
+from utils.decorators import Memoize
+import math
+import pdb
 
 
 def max_subarray(array):
@@ -38,8 +45,8 @@ def max_subarray(array):
     maximum_so_far = array[0]
     for item in array[1:]:
         # the trick comes from this line: at this point either the new item
-        # starts a new sequence or contribute to continue the current sequence,
-        # we select hichever case gives the best value
+        # starts a new sequence or contributes to continue the current sequence
+        # we select whichever case gives the best value
         maximum_ending_here = max(item, maximum_ending_here + item)
         maximum_so_far = max(maximum_so_far, maximum_ending_here)
     return maximum_so_far
@@ -64,9 +71,111 @@ def solve_knapsack(items, max_weight):
     raise NotImplementedError
 
 
-def change_making(target, coins):
+#------------------------------------------------------------------------------
+#  Change Making algorithms
+#------------------------------------------------------------------------------
+
+
+def change_making_greedy(target, coin_set):
     """
-    returns the minimal numbers of coins required to equal the target, or None
+    returns the minimal numbers of coins required to reach the target, or [] if
+    it can't be reached. Note that this algorithm fails for special sets of
+    coins called "non-canonical" sets. Most real-life sets are canonical though
+    and we can use this efficient algorithm operating in
+    O(target/smallest_coin).
+    """
+    # We sort the coins and return early the trivial results (target below the
+    # minimum available coin or target already in the coin_set)
+    coins = sorted(list(coin_set))
+    if target < coins[0]:
+        return []
+    if target in coin_set:
+        return [target]
+    # while there is still some change to give, we select the largest coin we
+    # can return to pay this remaining change. We stop when there is no coin
+    # small enough to do this
+    still_to_give = target
+    coins_to_use = []
+    while still_to_give >= coins[0]:
+        max_usable_coin = max(coin for coin in coins if coin <= still_to_give)
+        still_to_give -= max_usable_coin
+        coins_to_use.append(max_usable_coin)
+    # if we managed to give the exact change, we return the coins we used
+    return coins_to_use if still_to_give == 0 else []
+
+
+def change_making_bottom_up(target, coin_set):
+    """
+    returns the minimal numbers of coins required to reach the target, or []
     if the target cannot be reached.
     """
-    raise NotImplementedError
+    # We sort the coins and return the trivial results (target below the
+    # minimum available coin or target already in the coin_set)
+    coins = sorted(list(coin_set))
+    if target < coins[0]:
+        return []
+    if target in coin_set:
+        return [target]
+
+    # Init two lists to store the minimum number of coins and their nature,
+    # the index being the change_required
+    coin_count = [0] * (target + 1)
+    best_combo = [[] for _ in range(target + 1)]
+
+    # Looping over the smallest coin up to the target
+    for change in range(coins[0], target + 1):
+        # we return faster if the change is a coin within the coin_set
+        if change in coin_set:
+            coin_count[change], best_combo[change] = 1, [change]
+            continue
+        # otherwise we locate the minimum number of coins by finding the coin
+        # which added to change - coin is optimal
+        minimum_number = math.inf
+        minimum_coin = None
+        for coin in coins:
+            is_minimum = (
+                change > coin and coin_count[change - coin] != 0 and
+                coin_count[change - coin] + 1 < minimum_number
+            )
+            if is_minimum:
+                minimum_number = coin_count[change - coin] + 1
+                minimum_coin = coin
+
+        # If we found a minimum coin we update the best_combo and coin_count
+        # (this may not be the case for some sets not containing 1)
+        if minimum_coin:
+            best_combo[change] = (best_combo[change - minimum_coin] +
+                                   [minimum_coin])
+            coin_count[change] = coin_count[change - minimum_coin] + 1
+
+    return best_combo[target]
+
+
+@Memoize
+def change_making_top_down(target, coin_set):
+    """
+    returns the minimal numbers of coins required to equal the target, or []
+    if the target cannot be reached. Note that the coin_set has to be of type
+    set or frozenset since we use memoization: the arguments must be hashable
+    and a list is not.
+    """
+    # We sort the coins and return the trivial results (target below the
+    # minimum available coin or target already in the coin_set)
+    coins = sorted(list(coin_set))
+    if target < coins[0]:
+        return []
+    if target in coin_set:
+        return [target]
+
+    # we initiate a list representing the combination giving the minimum change
+    # we then loop over each coin to find if adding this coin to the ways of
+    # returning target - coin leads to a new minimum
+    best_combo = [0] * (target // coins[0] + 1)
+    for coin in coins:
+        if target - coin < 0:
+            continue
+        combo = change_making_top_down(target - coin, coin_set) + [coin]
+        if len(combo) < len(best_combo):
+            best_combo = combo
+    # we make sure we have reached the target before returning the combo
+    return best_combo if sum(best_combo) == target else []
--- src/classic_algo/tests/test_dynamic.py
+++ src/classic_algo/tests/test_dynamic.py
@@ -2,7 +2,7 @@ from ..dynamic import (
     max_subarray,
     longest_common_subsequence,
     solve_knapsack,
-    change_making,
+    change_making_bottom_up, change_making_top_down, change_making_greedy,
 )
 
 
@@ -23,3 +23,79 @@ def test_max_subarray():
     assert max_subarray([3, -2, 1, 5, 1, -2, 2, 4]) == 12
     # Mix of positive and negative, broken sequence
     assert max_subarray([3, -22, 1, 5, 1, -32, 2, 4]) == 7
+
+
+# -----------------------------------------------------
+#       Change making
+# -----------------------------------------------------
+
+
+def test_canonical_change_making():
+    """
+    Tests for a "canonical" coin set, which corresponds to real-life situation.
+    All changes can be returned and the greedy algorithm returns the optimal
+    solution
+    """
+    coin_set = frozenset({1, 2, 5, 10, 20, 50, 100})
+    # Testing target lower than minimum coin
+    assert change_making_greedy(0, coin_set) == []
+    assert change_making_bottom_up(0, coin_set) == []
+    assert change_making_top_down(0, coin_set) == []
+    # Testing target doable with one single coin
+    assert change_making_greedy(10, coin_set) == [10]
+    assert change_making_bottom_up(10, coin_set) == [10]
+    assert change_making_top_down(10, coin_set) == [10]
+    # Testing target doable with two coins
+    assert sorted(change_making_greedy(12, coin_set)) == [2, 10]
+    assert sorted(change_making_bottom_up(12, coin_set)) == [2, 10]
+    assert sorted(change_making_top_down(12, coin_set)) == [2, 10]
+    # Testing target reachable with 3 coins
+    assert sorted(change_making_greedy(14, coin_set)) == [2, 2, 10]
+    assert sorted(change_making_bottom_up(14, coin_set)) == [2, 2, 10]
+    assert sorted(change_making_top_down(14, coin_set)) == [2, 2, 10]
+    # Target reachable with 3 coins, all different
+    assert sorted(change_making_greedy(16, coin_set)) == [1, 5, 10]
+    assert sorted(change_making_bottom_up(16, coin_set)) == [1, 5, 10]
+    assert sorted(change_making_top_down(16, coin_set)) == [1, 5, 10]
+
+
+def test_change_making_set_without_unit():
+    """
+    Tests for a coin_set which do not contain 1. In this case, there exist
+    changes which cannot be returned
+    """
+    coin_set = frozenset({5, 10, 20, 50, 100})
+    # Testing target lower than minimum coin
+    assert change_making_greedy(16, coin_set) == []
+    assert change_making_bottom_up(16, coin_set) == []
+    assert change_making_top_down(16, coin_set) == []
+
+
+
+def test_non_canonical_change_making():
+    """
+    Non-canonical coin set are sets for which the greedy algorithm is not
+    guaranteed to return the optimal solution.
+    """
+    coin_set = frozenset({1, 3, 4})
+    # Test showing the failure of the grredy algorithm for a non-canonical set,
+    # it does not always return the minimum result
+    assert sorted(change_making_greedy(6, coin_set)) == [1, 1, 4]
+    assert sorted(change_making_bottom_up(6, coin_set)) == [3, 3]
+    assert sorted(change_making_top_down(6, coin_set)) == [3, 3]
+    # Testing target lower than minimum coin
+    assert change_making_greedy(0, coin_set) == []
+    assert change_making_bottom_up(0, coin_set) == []
+    assert change_making_top_down(0, coin_set) == []
+    # Testing target doable with one single coin
+    assert change_making_greedy(4, coin_set) == [4]
+    assert change_making_bottom_up(4, coin_set) == [4]
+    assert change_making_top_down(4, coin_set) == [4]
+    # Testing target doable with two coins
+    assert sorted(change_making_greedy(7, coin_set)) == [3, 4]
+    assert sorted(change_making_bottom_up(7, coin_set)) == [3, 4]
+    assert sorted(change_making_top_down(7, coin_set)) == [3, 4]
+    # Testing target reachable with 3 coins
+    assert sorted(change_making_greedy(11, coin_set)) == [3, 4, 4]
+    assert sorted(change_making_bottom_up(11, coin_set)) == [3, 4, 4]
+    assert sorted(change_making_top_down(11, coin_set)) == [3, 4, 4]
